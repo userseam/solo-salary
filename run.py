4f94ceeb28a6d80c65e3947f9f23c659627fac4f
Morad Mouhttouch <moradmouhttouch@gmail.com>
2022-07-25 18:00:45 +0000

    added for loop to go trow all the 5 week days and so that you can input a value for each day

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread
+
+print("Welcome to the solo salary application \n")
+
+def week_days():
+    """ 
+    This will be used to loop throw all the 5 days of the week and add them to a list
+    """
+    
+    
+    days = ["Monday" , "Tuseday" , "Wensday", "Thursday" , "Fryday"]
+    for i in days:
+        input(f"Did you work on  {i} ? \n")
+    
+    
+       
+
+week_days()
