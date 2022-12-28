2a8dead76192721f40f5f9776e75e51d55ae9edc
Morad Mouhttouch <moradmouhttouch@gmail.com>
2022-12-28 21:14:16 +0000

    have resolved problem with indentation and syntax , still a problem with input were the input value passes even tho its not yes or no

--- run.py
+++ run.py
@@ -3,57 +3,38 @@ from collections import Counter
 
 
 days = ("Monday", "Tuseday", "Wensday", "Thursday", "Fryday")
+
 yes_or_no = {}
 
+#stock = {"laptop": 150, "phone": 75, "TV": 100, "Coustom_PC": 225, "coffe_machine": 150, "frezzer": 100}
 
 
 def get_week_days():
-    print("Welcome to the solo salary application ! \n")
-    print("Enter either yes or no as an awnser... \n")
-    
 
+
+
+    print("✭  Welcome to the solo salary application ! ✭  \n")
+    print("Enter either yes or no as an awnser... \n")
     for i in days:
         """loop trow all the days in the days variabel"""   
-       
         """removes spaces , transform to lowercase to not raise error""" 
-        
         while True:
             print(f"Did you work on {i} ? \n")
             answer = input("\n").strip().lower()
-           
-            if not answer.isalpha() or answer not in ["yes", "no"]:
-                 print(f"Value Error : {answer} , please follow the instructions above and insert the correct input")
-                 print("__________________________")
-                 print(f"Did you work on {i} ? \n ")
-                 answer = input("\n").strip().lower()
+            if not answer.isalpha() or answer is ["yes","no"]:
+                print(f"Value Error : {answer} , please follow the instructions above and insert the correct input")
+                print(" ! __________________________ ! \n")    
+                print(f"    Did you work on {i} ? \n ")
+                print(" ! __________________________ ! \n")
             if answer.isalpha() or answer is ["yes", "no"]:
-                break   
-
-          
-                
-
-            
-            
-        
-        
-
-
-        
-        """
-            if the wrong user input is wrong , print message and validates that
-            the user input was in a str format and matching the expected awnser
-            if not , the user would be notefied and can try agian
-        """
-        
-
-
+                break
         yes_or_no[i] = answer
 
+#def sales_comision():
+#    for key, value in stock.items():
+        
 
-
-
-
-
+#sales_comision()    
 
 get_week_days()
 """ calling  the function for input and validation for what days the user have worked"""
@@ -62,11 +43,13 @@ get_week_days()
 work_week = Counter(yes_or_no.values())
 """checking the amount of yes values from the user input stored in the yes_or_no variabel"""
 
+
 days_worked = work_week["yes"]
 """extrecting the nummber of "yes":es and storeing it in a variabel"""
 
-print(f"You have worked for a total of {days_worked} this week! \n")
+print(f"You have worked for a total of {days_worked} days this week! \n")
 "telling the user on how many days of the week they have worked"
+
 
 payment = work_week["yes"] * 200
 """multeplying the days worked with the dayly salary"""
